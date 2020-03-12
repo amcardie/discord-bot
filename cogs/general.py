70ddc52e297303e4bb7faf6346972ce713b707ef
amcardie <62069983+amcardie@users.noreply.github.com>
2020-03-12 09:01:56 -0700

    Moved games from general cog to games cog

--- cogs/general.py
+++ cogs/general.py
@@ -46,49 +46,10 @@ class General(commands.Cog):
     @commands.command()
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def say(self, ctx, *, mes):
-        mes = mes.replace("@everyone", "@ everyone")
-        mes = mes.replace("@here", "@ here")
+        mes = mes.replace("@everyone", "@\u200beveryone")
+        mes = mes.replace("@here", "@\u200bhere")
         await ctx.message.delete()
         await ctx.send(mes)
-
-    @commands.command()
-    @commands.cooldown(1, 3, commands.BucketType.user)
-    async def flip(self, ctx):
-        hf = ["Heads", "Tails"]
-        ch = random.choice(hf)
-        await ctx.send(ch)
-
-    @commands.command()
-    @commands.cooldown(1, 3, commands.BucketType.user)
-    async def rps(self, ctx, *, choice = None):
-        if choice is None:
-            return await ctx.send("You need to pick from rock paper and scissors")
-
-        choices = ["rock", "paper", "scissors"]
-        pick = random.choice(choices)
-
-        if pick == choice.lower():
-            await ctx.send(f"You picked {choice} and I picked {pick}. Tie!")
-
-        if pick == "rock" and choice.lower() == "scissors":
-            await ctx.send(f"You picked {choice} and I picked {pick}. You Lose!")
-
-        if pick == "rock" and choice.lower() == "paper":
-            await ctx.send(f"You picked {choice} and I picked {pick}. You Win!")
-
-        if pick == "paper" and choice.lower() == "rock":
-            await ctx.send(f"You picked {choice} and I picked {pick}. You Lose!")
-
-        if pick == "paper" and choice.lower() == "scissors":
-            await ctx.send(f"You picked {choice} and I picked {pick}. You Win!")
-
-        if pick == "scissors" and choice.lower() == "rock":
-            await ctx.send(f"You picked {choice} and I picked {pick}. You Win!")
-
-        if pick == "scissors" and choice.lower() == "paper":
-            await ctx.send(f"You picked {choice} and I picked {pick}. You Lose!")
-        
-
 
 def setup(bot):
     bot.add_cog(General(bot))
