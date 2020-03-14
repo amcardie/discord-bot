16aaf2485420d237f9099ac52946b1bcf805ecec
amcardie <62069983+amcardie@users.noreply.github.com>
2020-03-14 02:30:41 -0700

    Added 10s timer test game
    Tried making tic-tac-toe but couldn't figure it fully out unfortunately

--- cogs/games.py
+++ cogs/games.py
@@ -1,11 +1,38 @@
 import discord
 from discord.ext import commands
 import random
+from datetime import datetime
+
 
 class Games(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+    
+    # I saw a timer test thingy in the discord.py discord server a while ago so I wanna try and re-make that
+    @commands.command(aliases=["10s", "10"])
+    @commands.cooldown(1, 3, commands.BucketType.user)
+    async def ten(self, ctx):
+        embed = discord.Embed(colour=discord.Colour.purple(), title="10s timer test")
+        ft = "gif" if ctx.author.is_avatar_animated() else "png"
+        embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url_as(format=ft))
+        mes = await ctx.send(embed=embed)
+        await mes.add_reaction("⏲️")
 
+        def check(reaction, user):
+            return user == ctx.author and str(reaction.emoji) == "⏲️"
+
+        await self.bot.wait_for('reaction_add', check=check)
+        begin = mes.created_at
+        end = datetime.utcnow()
+        final = end - begin
+        res = f"{final.seconds} seconds and " + f"{final.microseconds}"[:2]
+
+        # new_embed = discord.Embed(colour=discord.Colour.purple(), title=f"{res}")
+        # new_embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url_as(format=ft)
+        embed.add_field(name="Results", value=f"{res} milliseconds")
+        await mes.edit(embed=embed)
+        # await ctx.send(embed=new_embed)
+ 
     @commands.command()
     @commands.cooldown(1, 3, commands.BucketType.user)
     async def flip(self, ctx):
