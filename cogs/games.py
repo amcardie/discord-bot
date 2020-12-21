5cef29eff59b10c4b8a987e9639f32364a0caae4
amcardie <62069983+amcardie@users.noreply.github.com>
2020-12-21 03:46:31 -0800

    Making rps look better.

--- cogs/games.py
+++ cogs/games.py
@@ -10,6 +10,9 @@ class Games(commands.Cog):
     @commands.command()
     @commands.cooldown(1, 3, commands.BucketType.user)
     async def flip(self, ctx):
+        """
+        Flips heads or tails.
+        """
         hf = ["Heads", "Tails"]
         ch = random.choice(hf)
         await ctx.send(ch)
@@ -17,32 +20,44 @@ class Games(commands.Cog):
     @commands.command()
     @commands.cooldown(1, 3, commands.BucketType.user)
     async def rps(self, ctx, *, choice = None):
+        """
+        Plays a game of rock paper scissors.
+        """
         if choice is None:
             return await ctx.send("You need to pick from rock paper and scissors")
 
         choices = ["rock", "paper", "scissors"]
         pick = random.choice(choices)
 
+        embed = discord.Embed(colour=discord.Colour.purple(), title="Rock Paper Scissors")
+
         if pick == choice.lower():
-            await ctx.send(f"You picked {choice} and I picked {pick}. Tie!")
+            embed.add_field(name="Tie!", value=f"`You picked {choice} and I picked {pick}.`")
+            await ctx.send(embed=embed)
 
         elif pick == "rock" and choice.lower() == "scissors":
-            await ctx.send(f"You picked {choice} and I picked {pick}. You Lose!")
+            embed.add_field(name="You Lose!", value=f"`You picked {choice} and I picked {pick}.`")
+            await ctx.send(embed=embed) 
 
         elif pick == "rock" and choice.lower() == "paper":
-            await ctx.send(f"You picked {choice} and I picked {pick}. You Win!")
+            await embed.add_field(name="You Win!", value=f"`You picked {choice} and I picked {pick}.`")
+            await ctx.send(embed=embed) 
 
         elif pick == "paper" and choice.lower() == "rock":
-            await ctx.send(f"You picked {choice} and I picked {pick}. You Lose!")
+            embed.add_field(name="You Lose!", value=f"`You picked {choice} and I picked {pick}.`")
+            await ctx.send(embed=embed) 
 
         elif pick == "paper" and choice.lower() == "scissors":
-            await ctx.send(f"You picked {choice} and I picked {pick}. You Win!")
+            embed.add_field(name="`You Win!", value=f"You picked {choice} and I picked {pick}.`")
+            await ctx.send(embed=embed) 
 
         elif pick == "scissors" and choice.lower() == "rock":
-            await ctx.send(f"You picked {choice} and I picked {pick}. You Win!")
-
+            embed.add_field(name="You Win!", value=f"`You picked {choice} and I picked {pick}.`")
+            await ctx.send(embed=embed) 
+            
         else:
-            await ctx.send(f"You picked {choice} and I picked {pick}. You Lose!")
+            await embed.add_field(name="You Lose!", value=f"`You picked {choice} and I picked {pick}.`")
+            await ctx.send(embed=embed) 
 
 def setup(bot):
     bot.add_cog(Games(bot))
