b218da701c24bf08dc0b27b3f9b01ba549b9439a
amcardie <62069983+amcardie@users.noreply.github.com>
2021-01-11 06:00:18 -0800

    Fixed many issues
    Removed enabling/disabling logs and optimized stuff

--- cogs/logging.py
+++ cogs/logging.py
@@ -15,36 +15,9 @@ class Logging(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(invoke_without_command=True)
+    @commands.command()
     @commands.has_permissions(manage_guild=True)
-    async def log(self, ctx):
-        return await ctx.send(f"Availabe log commands: channel, enable, disable. Note: Log comes defaulted as disabled so you must do {ctx.prefix}log enable after/before setting channel")
-    
-    # @log.command()
-    # @commands.has_permissions(manage_guild=True)
-    # async def channel(self, ctx, channel: discord.TextChannel = None):
-    #     """
-    #     Sets the channel for the logs to be sent to.
-    #     """
-    #     if channel is None:
-    #       return await ctx.send("Please specify a channel to send logs to")
-    #     ap = {
-    #         "server_id": str(ctx.guild.id),
-    #         "channel_id": str(channel.id)
-    #     }
-
-    #     with open("logging.json") as json_file:
-    #         data = json.load(json_file)
-    #         temp = data["logs"]
-    #         y = ap
-    #         write_json(data)
-
-    #         temp.append(y)
-    #         write_json(data)
-    
-    @log.command()
-    @commands.has_permissions(manage_guild=True)
-    async def enable(self, ctx, channel: discord.TextChannel = None):
+    async def log(self, ctx, channel: discord.TextChannel = None):
         """
         Enables Logging.
         """
@@ -54,46 +27,17 @@ class Logging(commands.Cog):
         ap = {
             "server_id": str(ctx.guild.id),
             "channel_id": str(channel.id),
-            "log": "True",
         }
-        with open("logging.json") as json_file:
-            data = json.load(json_file)
-            for key in data["logs"]:
-                 if key['server_id'] == str(ctx.guild.id):
-                     if key["log"] == "True":
-                        return await ctx.send("Logging already enabled")
-                        
-            temp = data["logs"]
-            y = ap
-            write_json(data)
+        with open("logging.json") as pre:
+            data = json.load(pre)
+            
+        temp = data["logs"]
+        y = ap
+        write_json(data)
 
-            temp.append(y)
-            write_json(data)
-    
-    # @log.command()
-    # @commands.has_permissions(manage_guild=True)
-    # async def disable(self, ctx):
-    #     """
-    #     Disables Logging.        
-    #     """
-    #     ap = {
-    #         "server_id": str(ctx.guild.id),
-    #         "log": "False",
-    #     }
-    #     with open("logging.json") as json_file:
-    #         data = json.load(json_file)
-    #         for key in data["logs"]:
-    #             if key['server_id'] == str(ctx.guild.id):
-    #                 if key["log"] == "False":
-    #                     return await ctx.send("Logging already disabled")
-    #                 else:
-    #                     key['log'] = "False"
-    #                     write_json(data)
-    #                     return
-        
-    #         data.append(ap)
-    #         write_json(ap)
-
+        temp.append(y)
+        write_json(data)
+   
     @commands.Cog.listener()
     async def on_message_delete(self, message):
         if message.author.id == self.bot.user.id:
@@ -101,18 +45,16 @@ class Logging(commands.Cog):
         with open("logging.json") as pre:
             data = json.load(pre)
             for key in data["logs"]:
-                if key["server_id"] == str(message.guild.id) and key["log"] == "True":
+                if key["server_id"] == str(message.guild.id):
                     ft = "gif" if message.author.is_avatar_animated() else "png"
                     now = datetime.now()
                     embed = discord.Embed(title=f"Message deleted in {message.channel.name}", colour=discord.Colour.purple())
                     embed.set_author(name=message.author.name, icon_url=message.author.avatar_url_as(format=ft))
                     embed.add_field(name=f"**{message.content}**", value=f"\u200b")
                     embed.set_footer(text=f"Author ID: {message.author.id} | Message ID: {message.id}\nChannel ID: {message.channel.id} | Time: {now.strftime('%Y-%m-%d %I:%M:%S')}")
-                    for key in data["logs"]:
-                        if key['server_id'] == str(message.guild.id):
-                            chid = int(key['channel_id'])
-                            channel = self.bot.get_channel(chid)
-                            return await channel.send(embed=embed)
+                    chid = int(key['channel_id'])
+                    channel = self.bot.get_channel(chid)
+                    return await channel.send(embed=embed)
     
     @commands.Cog.listener()
     async def on_message_edit(self, before, after):
@@ -121,7 +63,7 @@ class Logging(commands.Cog):
         with open("logging.json") as pre:
             data = json.load(pre)
             for key in data["logs"]:
-                if key["server_id"] == str(after.guild.id) and key["log"] == "True":
+                if key["server_id"] == str(after.guild.id):
                             ft = "gif" if after.author.is_avatar_animated() else "png"
                             now = datetime.now()
                             embed = discord.Embed(title=f"Message edited in {after.channel.name}", colour=discord.Colour.purple())
@@ -129,11 +71,9 @@ class Logging(commands.Cog):
                             embed.add_field(name="Before Edit:", value=before.content)
                             embed.add_field(name="After Edit:", value=after.content, inline=False)
                             embed.set_footer(text=f"Author ID: {after.author.id}\nChannel ID: {after.channel.id} | Time: {now.strftime('%Y-%m-%d %I:%M:%S')}")
-                            for key in data["logs"]:
-                                if key['server_id'] == str(after.guild.id):
-                                    chid = int(key['channel_id'])
-                                    channel = self.bot.get_channel(chid)
-                                    return await channel.send(embed=embed)
+                            chid = int(key['channel_id'])
+                            channel = self.bot.get_channel(chid)
+                            return await channel.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Logging(bot))
