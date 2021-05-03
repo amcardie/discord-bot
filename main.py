623965580d150c903354f01f5abd3eea4a2bf2c1
amcardie <62069983+amcardie@users.noreply.github.com>
2021-05-03 00:14:50 -0700

    Custom prefix per server

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 import logging
 import asyncio
 from discord.ext import commands
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -14,15 +14,19 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
+def get_prefix(bot, message):
+    with open("prefixes.json", "r") as f:
+        prefixes = json.load(f)
+
+    return prefixes[str(message.guild.id)]
 
 # Getting token and prefix from a seperate file
 with open("preferences.json") as f:
     t = json.load(f)
     token = t["token"]
-    prefix = t["prefix"]
 
 # Setting up the bot
-bot = commands.Bot(command_prefix=prefix, intents=intents)
+bot = commands.Bot(command_prefix=get_prefix, intents=intents)
 
 # From https://gist.github.com/InterStella0/b78488fb28cadf279dfd3164b9f0cf96
 class CustomHelp(commands.MinimalHelpCommand):
@@ -53,13 +57,46 @@ async def ping(ctx):
     duration = (end - start) * 1000
     await message.edit(content='Pong! {:.2f}ms'.format(duration))
 
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def prefix(ctx, prefix = None):
+    with open("prefixes.json", "r") as f:
+        prefixes = json.load(f)
+
+    if not prefix:
+        return await ctx.send(f"This servers prefix is `{ctx.prefix}`, to change it run the command again with the prefix you want at the end of the command. To add a space put space at the end of the prefix like [prefixspace]")
+    
+    if prefix.endswith("space"):
+        prefix = prefix.replace("space", " ")
+    
+    prefixes[str(ctx.guild.id)] = prefix
+
+    with open("prefixes.json", "w") as f:
+        json.dump(prefixes, f)
+    
+    await ctx.send(f"This servers prefix has been changed to `{prefix}`")
+
+@bot.event
+async def on_guild_join(guild):
+    with open("prefixes.json", "r") as f:
+        prefixes = json.load(f)
+
+    prefixes[str(guild.id)] = ";"
+
+    with open("prefixes.json", "w") as f:
+        json.dump(prefixes, f)
+
 # The code above is pretty much the only thing thats gonna be in this file for now for commands i'm gonna be using cogs because its much faster
 # The code below is all for loading/unloading cogs
 
+cogs = []
 for cog in os.listdir("./cogs"):
-     if cog.endswith(".py"):
+    if cog.endswith(".py") and not cog.startswith("_"):
+        cogs.append(cog)
         bot.load_extension(f"cogs.{cog[:-3]}")
-        print(f"loaded cog: {cog}")
+
+print("loaded " + ', '.join(cogs))
+        
 
 @bot.command()
 @commands.has_permissions(administrator=True)
@@ -79,6 +116,8 @@ async def reload(ctx, ext = None):
     cogs = "" 
     if not ext:
         for cog in os.listdir("./cogs"):
+            if cog.startswith("_"):
+                continue
             if cog.endswith(".py"):
                 bot.reload_extension(f"cogs.{cog[:-3]}")
                 cogs += f"\n{cog[:-3].upper()} cog"
